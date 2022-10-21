1f87e65be67b2b047e574658f582655f33375e3f
daniilsjb <daniilb@outlook.com>
2022-10-21 13:48:25 +0300

    Add some fancy math

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+class Math {
+    public static int add(int... args) {
+        int result = 0;
+        for (var number : args) {
+            result += number;
+        }
+        return result;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, world!");
     }
 }
