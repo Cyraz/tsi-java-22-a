869d9b14f128dbff436c1baae45f0b5e68d86a7a
Shivam Rajput <srecho97@gmail.com>
2022-10-21 13:46:03 +0300

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        start();
         System.out.println("Hello world!");
+    }
+
+    public static void start() {
+        System.out.println("Application started");
     }
 }
